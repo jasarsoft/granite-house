90377bf46f9b58207acde12ce8ddf0dcec7e473c
Edin Jašarević <edinjasar14@gmail.com>
2019-08-12 17:56:10 +0200

    product type - delete action method

--- Granite House/Areas/Admin/Controllers/ProductTypesController.cs
+++ Granite House/Areas/Admin/Controllers/ProductTypesController.cs
@@ -99,5 +99,34 @@ namespace Granite_House.Areas.Admin.Controllers
 
             return View(productType);
         }
+
+
+        //GET Delete Action Method
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        //POST Delete action method
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var productType = await _db.ProductTypes.FindAsync(id);
+            _db.ProductTypes.Remove(productType);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
