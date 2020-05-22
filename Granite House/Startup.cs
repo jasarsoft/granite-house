61300635dff2681a63f28f9bc5fc39636b215d3e
Edin Jašarević <edinjasar14@gmail.com>
2020-05-22 13:05:31 +0200

    shopping cart view part

--- Granite House/Startup.cs
+++ Granite House/Startup.cs
@@ -30,7 +30,7 @@ namespace Jasarsoft.GraniteHouse
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
@@ -45,6 +45,7 @@ namespace Jasarsoft.GraniteHouse
             services.AddSession(options =>
             {
                 options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.Name = "granite.house.session";
                 options.Cookie.HttpOnly = true;
             });
         }
