15a69ebc571231a601937f54e28d2790783940b4
Edin Jašarević <edinjasar14@gmail.com>
2020-06-06 15:28:49 +0200

    users - scoffold identity to project

---
+++ Granite House/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Jasarsoft.GraniteHouse.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Jasarsoft.GraniteHouse.Areas.Identity.IdentityHostingStartup))]
+namespace Jasarsoft.GraniteHouse.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
