73bdd1f46740fe2c8e209e52b99b3507c5c0525d
Edin Jašarević <edinjasar14@gmail.com>
2020-06-07 00:26:10 +0200

    appointments index get action method

--- Granite House/Areas/Admin/Controllers/AppointmentsController.cs
+++ Granite House/Areas/Admin/Controllers/AppointmentsController.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Jasarsoft.GraniteHouse.Data;
+using Jasarsoft.GraniteHouse.Models;
+using Jasarsoft.GraniteHouse.Models.ViewModel;
 using Jasarsoft.GraniteHouse.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Jasarsoft.GraniteHouse.Areas.Admin.Controllers
 {
@@ -12,9 +17,33 @@ namespace Jasarsoft.GraniteHouse.Areas.Admin.Controllers
     [Area("Admin")]
     public class AppointmentsController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public AppointmentsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            ClaimsPrincipal currentUser = this.User;
+            var claimsIdentity = (ClaimsIdentity) this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            AppointmentViewModel appointmentVM = new AppointmentViewModel()
+            {
+                Appointmentses = new List<Appointments>()
+            };
+
+            appointmentVM.Appointmentses = _db.Appointmentses.Include(a => a.SalesPersion).ToList();
+            if (User.IsInRole(SD.AdminEndUser))
+            {
+                appointmentVM.Appointmentses =
+                    appointmentVM.Appointmentses.Where(a => a.SalesPersonId == claim.Value).ToList();
+
+            }
+
+            return View(appointmentVM);
         }
     }
 }
