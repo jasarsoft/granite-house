9581c54025e674da6a6a47f1875fc90436aab874
Edin Jašarević <edinjasar14@gmail.com>
2020-06-27 00:24:49 +0200

    appointment details view

---
+++ Granite House/Models/ViewModel/AppointmentDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jasarsoft.GraniteHouse.Models.ViewModel
+{
+    public class AppointmentDetailsViewModel
+    {
+        public Appointments Appointment { get; set; }
+        public List<ApplicationUser> SalesPersion { get; set; }
+        public List<Products> Products { get; set; }
+    }
+}
