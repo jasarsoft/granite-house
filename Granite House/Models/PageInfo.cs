7fe2eb020817105ee0be43e280291b2f5fc4182f
Edin Jašarević <edinjasar14@gmail.com>
2020-07-10 22:24:13 +0200

    paging info model

---
+++ Granite House/Models/PageInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jasarsoft.GraniteHouse.Models
+{
+    public class PageInfo
+    {
+        public int TotalItems { get; set; }
+
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int totalPage => (int) Math.Ceiling((decimal) TotalItems / ItemsPerPage);
+
+        //this will be used to bild url
+        public string urlParam { get; set; }
+    }
+}
