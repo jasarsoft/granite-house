ea1649e4704542a47be253717af636879cda4b9a
Edin Jašarević <edinjasar14@gmail.com>
2020-07-17 22:19:28 +0200

    seed database

---
+++ Granite House/Data/DbInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jasarsoft.GraniteHouse.Models;
+using Jasarsoft.GraniteHouse.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jasarsoft.GraniteHouse.Data
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async void Initialize()
+        {
+            _db.Database.Migrate();
+
+            if (_db.Roles.Any(r => r.Name == SD.SuperAdminEndUser)) return;
+
+            _roleManager.CreateAsync(new IdentityRole(SD.AdminEndUser)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.SuperAdminEndUser)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                Name = "Admin Test",
+                EmailConfirmed = true
+            }, "Admin123*").GetAwaiter().GetResult();
+
+            await _userManager.AddToRoleAsync(await _userManager.FindByEmailAsync("admin@gmail.com"),
+                SD.SuperAdminEndUser);
+        }
+    }
+}
--- Granite House/Startup.cs
+++ Granite House/Startup.cs
@@ -42,6 +42,8 @@ namespace Jasarsoft.GraniteHouse
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
 
+            services.AddScoped<IDbInitializer, DbInitializer>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddSession(options =>
@@ -53,7 +55,7 @@ namespace Jasarsoft.GraniteHouse
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IDbInitializer dbInitializer)
         {
             if (env.IsDevelopment())
             {
@@ -69,6 +71,8 @@ namespace Jasarsoft.GraniteHouse
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            dbInitializer.Initialize();
 
             app.UseAuthentication();
             app.UseSession();
